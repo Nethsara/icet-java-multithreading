7a2705ad942c4ebd348d1abcaa574142ca916ce1
Nethsara <nethsarasiyum2@gmail.com>
2023-05-15 12:15:33 +0530

    Time join

--- Demo.java
+++ Demo.java
@@ -6,10 +6,10 @@ class MyThread extends Thread {
 	public void run() {
 		for (int i = 0; i < 100; i++) {
 			System.out.println(getName() + " : " + i);
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException ex) {
-			}
+			// try {
+			// Thread.sleep(100);
+			// } catch (InterruptedException ex) {
+			// }
 		}
 	}
 }
@@ -17,19 +17,24 @@ class MyThread extends Thread {
 class Demo {
 	public static void main(String args[]) {
 		MyThread t1 = new MyThread("One");
+		MyThread t2 = new MyThread("Two");
+		MyThread t3 = new MyThread("Three");
+
+		System.out.println("State t1 : " + t1.getState());
+		System.out.println("State t2 : " + t2.getState());
+		System.out.println("State t3 : " + t3.getState());
+
 		t1.start();
-		for (int i = 0; i < 100; i++) {
-			System.out.println("main : " + i);
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException ex) {
-			}
-			if (i == 50) {
-				try {
-					t1.join();
-				} catch (InterruptedException ex) {
-				}
-			}
+		t2.start();
+		t3.start();
+
+		try {
+			t1.join();
+		} catch (Exception e) {
 		}
+
+		System.out.println("State t1 : " + t1.getState());
+		System.out.println("State t2 : " + t2.getState());
+		System.out.println("State t3 : " + t3.getState());
 	}
 }
