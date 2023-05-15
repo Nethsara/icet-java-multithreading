4c064a93e1af49ce97370cec61437bd59d013bc8
Nethsara <nethsarasiyum2@gmail.com>
2023-05-15 10:42:52 +0530

    thread refering

--- Demo.java
+++ Demo.java
@@ -1,46 +1,14 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-
-class MyFrame extends JFrame {
-	private JButton exitButton;
-	private JTextField text;
-
-	MyFrame() {
-		setSize(300, 300);
-		setTitle("MyFrame");
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setLocationRelativeTo(null);
-		setLayout(new FlowLayout());
-
-		text = new JTextField(10);
-		text.setFont(new Font("", 1, 30));
-		add(text);
-
-		exitButton = new JButton();
-		exitButton.setText("Exit");
-		exitButton.setFont(new Font("", 1, 30));
-		exitButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				String name = text.getText();
-				class MyThread extends Thread {
-					public void run() {
-						int i = 0;
-						while (true) {
-							System.out.println(name + " : " + i++);
-						}
-					}
-				}
-				new MyThread().start();
-
-			}
-		});
-		add(exitButton);
+class MyThread extends Thread {
+	public void run() {
+		Thread t = currentThread();
+		System.out.println("Name : " + t.getName());
 	}
 }
 
 class Demo {
 	public static void main(String args[]) {
-		new MyFrame().setVisible(true);
+		MyThread t1 = new MyThread();
+		t1.setName("My Thread");
+		t1.start(); // vs t1.run();
 	}
 }
