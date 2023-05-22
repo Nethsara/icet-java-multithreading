369f291e577e662615ed6d455485a5cc5370f108
Nethsara <nethsarasiyum2@gmail.com>
2023-05-22 09:49:39 +0530

    thread safety

--- Demo.java
+++ Demo.java
@@ -22,16 +22,39 @@ class CustomerController {
 		}
 		System.out.println("]");
 	}
+
+	static synchronized public void addOrder(String id, String name) {
+		System.out.print("{");
+		try {
+			Thread.sleep(500);
+		} catch (Exception ex) {
+		}
+		System.out.print(id);
+		try {
+			Thread.sleep(500);
+		} catch (Exception ex) {
+		}
+		System.out.print("-");
+		try {
+			Thread.sleep(500);
+		} catch (Exception ex) {
+		}
+		System.out.print(name);
+		try {
+			Thread.sleep(500);
+		} catch (Exception ex) {
+		}
+		System.out.println("}");
+	}
 }
 
 class Demo {
 	public static void main(String args[]) {
 		CustomerController c1 = new CustomerController();
-		Object ob = new Object();
 		new Thread() {
 			public void run() {
 				//
-				synchronized (ob) {
+				synchronized (CustomerController.class) {
 					c1.addCustomer("C001", "Danapala");
 				}
 				//
@@ -39,9 +62,7 @@ class Demo {
 		}.start();
 		new Thread() {
 			public void run() {
-				synchronized (ob) {
-					c1.addCustomer("C002", "Gunapala");
-				}
+				c1.addOrder("P001", "Suger");
 			}
 		}.start();
 	}
