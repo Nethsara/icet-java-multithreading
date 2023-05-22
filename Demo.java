2c3c32767806d0602da071e66bd741983dfe4faf
Nethsara <nethsarasiyum2@gmail.com>
2023-05-22 10:34:57 +0530

    dead lock

--- Demo.java
+++ Demo.java
@@ -1,39 +1,27 @@
-class Bucket {
-	synchronized public void get(int i) {
-		notify();
-		System.out.println("get : " + i);
-		try {
-			wait();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-	synchronized public void put(int i) {
-		notify();
-		System.out.println("put : " + i);
-		try {
-			wait();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+class A {
 }
 
 class Demo {
 	public static void main(String args[]) {
-		Bucket b1 = new Bucket();
+		A a1 = new A();
+		A a2 = new A();
 		new Thread() {
 			public void run() {
-				for (int i = 0; i < 50; i++) {
-					b1.get(i);
+				synchronized (a1) {
+					// yield or sleep
+					synchronized (a2) {
+						//
+					}
 				}
 			}
 		}.start();
 		new Thread() {
 			public void run() {
-				for (int i = 0; i < 50; i++) {
-					b1.put(i);
+				synchronized (a2) {
+					// yield or sleep
+					synchronized (a1) {
+						//
+					}
 				}
 			}
 		}.start();
